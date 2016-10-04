c3420f6d1883288aec839bd011df9a7e23ff8534
Benjamin Mukasa <bmukasa@advance.net>
2016-10-04 15:16:32 -0400

    Update index name

--- project/conf/base.py
+++ project/conf/base.py
@@ -207,6 +207,6 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'students',
+        'INDEX_NAME': 'directory',
     },
 }
