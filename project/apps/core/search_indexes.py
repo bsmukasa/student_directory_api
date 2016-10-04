3789bec96de5e9e5e2adad15ff645de91f85f725
Benjamin Mukasa <bmukasa@advance.net>
2016-10-04 16:19:56 -0400

    Fix search index bugs

--- project/apps/core/search_indexes.py
+++ project/apps/core/search_indexes.py
@@ -1,11 +1,13 @@
 from haystack import indexes
-from apps.core.models import Student, University
+from .models import Student, University
 
 
 class StudentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='full_name')
     first_name = indexes.CharField(model_attr='first_name')
     last_name = indexes.CharField(model_attr='last_name')
+    university = indexes.CharField(model_attr='university')
 
     def get_model(self):
         return Student
@@ -18,9 +20,13 @@ class UniversityIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name')
     state = indexes.CharField(model_attr='state')
+    students = indexes.MultiValueField()
 
     def get_model(self):
         return University
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
+
+    def prepare_students(self, obj):
+        return [student.full_name() for student in obj.students.all()]
